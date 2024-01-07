9286a3857279dd6474fb0e9f3c917368e14b71cd
Cleuber Maciel <105437987+cleuber979@users.noreply.github.com>
2024-01-07 17:12:41 -0300

    Add files via upload

---
+++ Tipo_Inteiro_Php.php
@@ -0,0 +1,14 @@
+<div claas="titulo">Tipo inteiro no PHP</div>
+
+<?php
+echo 11;
+echo '<br>';
+
+var_dump(11);
+echo'<br>';
+
+echo PHP_INT_MAX,'<BR>';
+echo PHP_INT_MAX,'<BR>';
+echo 017,<'br>';  //base octal
+echo 0b11000111001,<'br>';  // Base Binaria 
+echo 0X117acf0;  // base hexdecimal
